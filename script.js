1fee0e15c7c4548e8167bbe85a42b95ec9d81af9
Griffin Hammer <grifhammer@gmail.com>
2015-10-30 17:04:04 -0400

    Multiple Image Support Complete
    Completed multiple image support

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
 $(window).load(function(){
 	var	imageElements = $('.element-wrapper');
 	var rescaleImageDiv = $(imageElements).find('.image-wrapper');
-	for(i = 0; i < rescaleImageDiv.length; i++){
+	for(divCounter = 0; divCounter < rescaleImageDiv.length; divCounter++){
 		var imageHeights = [];
 		var imageWidths = [];
 		var maxWidth = 0;
-		var totalHeight;
+		var totalHeight = 0;
 
 		//Loop through all images in the image div to find total height and total width;
-		$(rescaleImageDiv[i]).find('img').each(function(){
+		$(rescaleImageDiv[divCounter]).find('img').each(function(){
 			imageHeights.push($(this).height());
 			imageWidths.push($(this).width());
 		});
 
-		for(i = 0; i < imageWidths.length; i++){
-			if(imageWidths[i] > maxWidth){
-				maxWidth = imageWidths[i];
+		for(widthCounter = 0; widthCounter < imageWidths.length; widthCounter++){
+			if(imageWidths[widthCounter] > maxWidth){
+				maxWidth = imageWidths[widthCounter];
 			}
 		}
-		for(i = 0; i < imageHeights.length; i++){
-			totalHeight += imageHeights[i];
+		for(heightsCounter = 0; heightsCounter < imageHeights.length; heightsCounter++){
+			totalHeight += imageHeights[heightsCounter];
 		}
 
-		if(totalHeight > totalWidth){
-
-
-			$(rescaleImageDiv[i]).height( 300 );
-			maxWidth = $(rescaleImages[i]).width();
-			$(rescaleImageDiv[i]).css('margin-left', (300-maxWidth) / 2);
-
-
+		if(totalHeight > maxWidth){
+			var heightRatios = 0;
+			var imagesInCurrDiv = $(rescaleImageDiv[divCounter]).find('img');
+			for(heightsCounter = 0; heightsCounter < imageHeights; heightsCounter++){
+				heightRatios = (imageHeights[heightsCounter]/totalHeight);
+				$(imagesInCurrDiv[heightsCounter]).css('height',300*heightRatios);
+			}
 
 		} else {
 
-
-			$(rescaleImageDiv[i]).width( 300 );
-			totalHeight = $(rescaleImageDiv[i]).height();
-			$(rescaleImageDiv[i]).find('img').first().css('margin-top', (300-totalHeight) / 2);
-
-
+			$(rescaleImageDiv[divCounter]).find('img').width(300);
+			imageHeights = [];
+			totalHeight = $(rescaleImageDiv[divCounter]).find('img').each(function(){
+				imageHeights.push($(this).height());
+			});
+			totalHeight = 0;
+			for(heightsCounter = 0; heightsCounter < imageHeights.length; heightsCounter++){
+				totalHeight += imageHeights[heightsCounter];
+			}
+			$(rescaleImageDiv[divCounter]).find('img:first-child').css('margin-top', ((300-totalHeight) / 2) );
+			$(rescaleImageDiv[divCounter]).find('img:first-child').css('float', 'left');
 		}
 	}
 });
+
+// get all image divs{
+// 	loop through images in div{
+// 		store the widths and heights
+// 	}
+// 	loop through height and width arrays{
+// 		find max width
+// 		find total height
+// 	}
+// 	if total height is greater than max width{
+// 		set height of each to the elements to their ratio of total * 300
+// 	} else{
+// 		set width of all images to 300 px
+// 	}
+// }
